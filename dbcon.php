873a28f21b006309b041e6be97c3ba4b3e50b403
DHEERAJ KUMAR GORLA <83914347+dheerajkumarmcr@users.noreply.github.com>
2024-05-30 10:32:33 +0530

    Add files via upload

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "transcen_admin");
+$con = mysqli_connect("localhost", "root", "", "transcen_admin");
 
 // Check connection
 if (mysqli_connect_errno())
